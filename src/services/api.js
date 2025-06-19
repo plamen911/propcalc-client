c0b6ddd7c9f9b08c5c67e67145ea2cad284dc539
Plamen Markov <plamen@lynxlake.org>
2025-06-19 23:47:18 +0300

    Changed to prod. API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'https://127.0.0.1:8000/'; // Base API URL
-// const API_URL = 'https://propcalc.zastrahovaite.com/'; // Base API URL
+//const API_URL = 'https://127.0.0.1:8000/'; // Base API URL
+const API_URL = 'https://propcalc.zastrahovaite.com/'; // Base API URL
 
 const api = axios.create({
   baseURL: API_URL,
